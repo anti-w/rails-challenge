30911e65267770565527d07c6b7e8d5898ad9407
Wendel Demetrio Bispo <wendel.bispo@dponet.com.br>
2023-04-24 11:08:20 -0300

    db seed

--- noob-code-mini-blog/db/seeds.rb
+++ noob-code-mini-blog/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+gamesCategory = Category.create(name: "Games")
+booksCategory = Category.create(name: "Books")
+moviesCategory = Category.create(name: "Movies")
+
+admin = User.create(name: 'Admin Boladão dono de tudo', password: '123456678', password_confirmation: '123456678', kind: 'AD', email: 'adminemail@admin.com')
+
+jornalist = User.create(name: 'Jornalista bom de bola', password: 'password', password_confirmation: 'password', kind: 'JO', email: 'jornalistemail@jornalist.com')
+
+gamesPost = Post.create(title: 'Elden Ring é pika irmaõ', description: 'Elden Ring vai te deixar maluco e tu vai querer chorar pra tua mãe, mas é gód.', category_id: gamesCategory.id, user_id: jornalist.id)
